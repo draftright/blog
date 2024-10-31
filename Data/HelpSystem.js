3dae80a81b65d49d571aeb916b2fb04cf2247872
draftright <draftright1@gmail.com>
2024-10-31 15:00:23 +0530

    Initial Hosting

---
+++ Data/HelpSystem.js
@@ -0,0 +1,10 @@
+var xmlHelpSystemData = "";
+xmlHelpSystemData += '<?xml version=\"1.0\" encoding=\"utf-8\"?>';
+xmlHelpSystemData += '<WebHelpSystem DefaultUrl=\"index.html\" Toc=\"Data/Tocs/TOC.js\" Index=\"Data/Index.js\" Concepts=\"Data/Concepts.js\" SearchDatabase=\"Data/Search.js\" Synonyms=\"Data/Synonyms.xml\" SearchUrl=\"Search.htm\" PathToScriptsFolder=\"Resources/Scripts/\" SkinName=\"Top-Navigation\" SkinID=\"Top_Navigation\" Multilingual=\"false\" Skins=\"Top-Navigation\" BuildTime=\"10/31/2024 2:57:53 PM\" BuildVersion=\"19.0.8524.35059\" TargetType=\"WebHelp2\" SkinTemplateFolder=\"Skin/\" InPreviewMode=\"false\" TopNavTocPath=\"false\" MoveOutputContentToRoot=\"true\" ReplaceReservedCharacters=\"false\" MakeFileLowerCase=\"false\" UseCustomTopicFileExtension=\"false\" PreventExternalUrls=\"false\" EnableResponsiveOutput=\"true\" IncludeGlossarySearchResults=\"true\" ResultsPerPage=\"20\" FeaturedSnippetsScopeID=\"AllFiles\" FeaturedSnippetsResults=\"1\" KnowledgePanelResults=\"1\" IncludeMicroContent=\"true\" xml:lang=\"en-us\" LanguageName=\"English\" SearchEngine=\"MadCapSearch\" IncludeCSHRuntime=\"true\" DebugMode=\"false\" ShowMadCapBacklink=\"false\" HasELearning=\"false\">';
+xmlHelpSystemData += '    <CatapultSkin Version=\"6\" SkinType=\"WebHelp2\" ComponentType=\"MasterPage\" TemplateName=\"Fluid\" Comment=\"HTML5 Top Navigation skin\" EnableResponsiveOutput=\"True\" AutoSyncTOC=\"true\" Name=\"Top-Navigation\" SkinID=\"Top_Navigation\" SkinClass=\"_Skins_Top_Navigation\" HideNavOnStartup=\"False\" LogoUrl=\"\">';
+xmlHelpSystemData += '        <WebHelpOptions NavigationPanePosition=\"Right\" OffCanvasMenuStyle=\"Accordion\" MainMenuPosition=\"Top\" />';
+xmlHelpSystemData += '        <MicroContentOptions FeaturedSnippetsViewMode=\"Truncated\" KnowledgePanelViewMode=\"Truncated\" />';
+xmlHelpSystemData += '        <ProxyOptions FAQProxyViewMode=\"DropDown\" KnowledgeProxyViewMode=\"Truncated\" PromotionProxyViewMode=\"PlainText\" />';
+xmlHelpSystemData += '    </CatapultSkin>';
+xmlHelpSystemData += '</WebHelpSystem>';
+MadCap.Utilities.Xhr._FilePathToXmlStringMap.Add('HelpSystem', xmlHelpSystemData);
---
+++ Resources/Scripts/require.config.js
@@ -0,0 +1,3 @@
+require.config({
+    urlArgs: 't=638659834739049196'
+});
